5bc65d943b6e3e03682b2cab56392b80e06cf99d
Luigi-Carneiro <120501789+Luigi-Carneiro@users.noreply.github.com>
2023-07-12 18:54:28 -0400

    Add files via upload

---
+++ 20230712/M = 9/funca.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "funca.h"
+
+void inicializar(TABELA tabela){
+    for (int i = 0; i < M; i++)
+        tabela[i] = NULL;
+}
+
+int chave(int num){
+    return (((num * 2) + 5) % M);
+}
+
+int inserir(TABELA tabela, int num){
+    int i = chave(num);
+    ELEMENTO *anterior = NULL;
+    ELEMENTO *e = tabela[i];
+    while(e != NULL){
+        if (e->numero == num)
+            return 0;
+        anterior = e;
+        e = e->proximo;
+    }
+    if(e == NULL){
+        e = malloc(sizeof(ELEMENTO));
+        e->numero = num;
+        e->proximo = NULL;
+        if(anterior == NULL)
+            tabela[i] = e;
+        else
+            anterior->proximo = e;
+    }
+    return 1;
+}
+
+void printHash(TABELA tabela){
+    ELEMENTO *e;
+    printf("\nTabela para M = 9:\n\n");
+    for(int i = 0; i < M; i++){
+        if (tabela[i] != NULL){
+            e = tabela[i];
+            printf("Posicao %d: ", i);
+            while(e != NULL){
+                printf("%d -> ", e->numero);
+                e = e->proximo;
+            }
+            printf("NULL\n");
+        }
+            else
+                printf("Posicao %d: NULL\n", i);
+    }
+}
+
